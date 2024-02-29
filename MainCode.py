3aaa0f1d12c0d8d8434c01a1960ab0bdb4963b3a
LoucasPladoCostante <161300918+LoucasPladoCostante@users.noreply.github.com>
2024-02-29 10:12:38 +0100

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -2,6 +2,8 @@
 """
 Created on Fri Feb 16 10:24:25 2024
 
+The following code compute the RMS at a given point of the structure
+
 @author: LP275843
 """
 import numpy as np
@@ -11,56 +13,65 @@ from Main.BeamParameters.BaseFunction import w
 from Main.PSD.Integrand import Integrand
 
 
+
+###############################################################################
+###############################################################################
+## All parameters have S.I. Units
+###############################################################################
+###############################################################################
+
+
+
 ###############################################################################
 ## Beam description
 ###############################################################################
 
 # Geometrical Constant
 
-L = 50*10**(-3)
-BeamWidth = 100*10**(-3)
-BeamThickness = 14.5*10**(-3)
+L = 50*10**(-3)                             # Length blade
+BeamWidth = 100*10**(-3)                    # Width blade
+BeamThickness = 14.5*10**(-3)               # Thickness blade
 
-R1 = 132*10**(-3)
-H = 515*10**(-3)
-CylinderThickness = 4*10**(-3)
+R1 = 132*10**(-3)                           # Radius inner cylinder
+H = 515*10**(-3)                            # Length inner cylinder
+CylinderThickness = 4*10**(-3)              # Thickness inner cylinder
 
-R2 = 150*10**(-3)
+R2 = 150*10**(-3)                           # Radius outer cylinder
 
-S = BeamWidth * BeamThickness
-I = ( BeamWidth * BeamThickness ** 3 ) / 12
+S = BeamWidth * BeamThickness               # Surface blade
+I = ( BeamWidth * BeamThickness ** 3 ) / 12 # Second moment of area
 
 # Material
 
-E = 2.10*10**11
-rho = 7850
+E = 2.10*10**11                             # Blade Young modulus
+rho = 7850                                  # Blade density
 
 # Structural damping
 
-Xi = 0
-Zeta = 5e-05
+Xi = 0                                      # Mass damping coefficient
+Zeta = 5e-05                                # Stifness damping coefficient
 
 # Mass
 
-Mcyl = 30
-Jcyl = Mcyl*((6*R1**2)+H**2)/12
+Mcyl = 30                                   # Mass inner cylinder 
+Jcyl = Mcyl*((6*R1**2)+H**2)/12             # Inertia inner cylinder 
 
 # Fluid
 
-rhoF = 1000
-nu = 10**(-6)
+rhoF = 1000                                 # Water density
+nu = 10**(-6)                               # Water viscosity
 
 # Flow description
 
-U = 2.6
+U = 2.6                                     # Mean flow velocity
 
 # Gravity
 
-G = 9.81
+G = 9.81                                    # Gravitational acceleration
 
 # Dimensional axial coordinate to compute the RMS
 
-X = L + H
+X = L + H                                   # Position where to compute the RMS
 
 beam = Beam(L, S, I, R1, H, R2, E, rho, Xi, Zeta, Mcyl, Jcyl, rhoF, nu, U, G)
 
